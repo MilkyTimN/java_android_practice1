288cdc755a9c6e20972ad696ea2f5960ca5f94d5
Tim <nsrv.tim@gmail.com>
2021-06-23 23:59:35 +0600

    Added variables

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -2,6 +2,15 @@ package exercise1;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        byte b = 8;
+        short s = 52;
+        int i = 21;
+        long l = 123123L;
+        float f = 123.23f;
+        double d = 1231.123123;
+        char c = 'a';
+        boolean isTrue = false;
+
     }
 }
