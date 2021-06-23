a27ebf33d8de3a1b8df608b7f24db37423cceed1
Tim <nsrv.tim@gmail.com>
2021-06-24 00:19:00 +0600

    Added exercise 3 : created method "calculator", 4 flout inited variables

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -3,14 +3,25 @@ package exercise1;
 public class Main {
     public static void main(String[] args) {
 
-        byte b = 8;
-        short s = 52;
-        int i = 21;
-        long l = 123123L;
-        float f = 123.23f;
-        double d = 1231.123123;
-        char c = 'a';
+        byte by = 8;
+        short sh = 52;
+        int in = 21;
+        long lo = 123123L;
+        float fl = 123.23f;
+        double dou = 1231.123123;
+        char ch = 'a';
         boolean isTrue = false;
 
+        float a = 12.3f;
+        float b = 44.1f;
+        float c = 9.12f;
+        float d = 71.0f;
+
+
+    }
+    public float calculator(float a, float b, float c, float d) {
+        float result = a * (b + (c / d));
+        return result;
     }
 }
+
