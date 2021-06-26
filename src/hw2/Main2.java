cd796fc8f80b6461648d3426ae9406b099fa334b
Tim <nsrv.tim@gmail.com>
2021-06-26 18:58:10 +0600

    HW2 Ex3

--- src/hw2/Main2.java
+++ src/hw2/Main2.java
@@ -11,13 +11,19 @@ public class Main2 {
                 array[i] = 1;
             }
         }
-        for (int a: array) {
-            System.out.println(a);
-        }
+
 //Ex2
         int[] array2 = new int[8];
         for (int i = 0, k = 0; i < array2.length; i++, k = i * 3) {
             array2[i] = k;
+        }
+
+//Ex3
+        int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array3.length; i++) {
+            if (array3[i] < 6) {
+                array3[i] *= 2;
+            }
         }
     }
 }
